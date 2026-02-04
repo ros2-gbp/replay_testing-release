bcd9242a4dcfebdc0a8fcc56b4db77808a89d59a
Troy Gibb <troy.gibb@gmail.com>
2026-02-04 11:57:32 -0800

    Rebase from 'debian/rolling/replay_testing'

--- replay_testing/filter.py
+++ replay_testing/filter.py
@@ -13,23 +13,38 @@
 # limitations under the License.
 #
 
-import subprocess
+import rosbag2_py
 
 
 def filter_mcap(input, output, output_topics):
-    command = [
-        'mcap',
-        'filter',
-        input,
-        '-o',
-        output,
-    ]
+    """Filter out specified topics from an mcap file.
 
-    # Filter out topics that will be replayed
-    for topic in output_topics:
-        command.extend(['-n', topic])
+    Args:
+        input: Path to input mcap file
+        output: Path to output mcap file
+        output_topics: List of topic names to exclude from the output
+    """
+    topics_to_exclude = set(output_topics)
 
-    try:
-        subprocess.run(command, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f'Error filtering {input}: {e}')
+    reader = rosbag2_py.SequentialReader()
+    reader.open(
+        rosbag2_py.StorageOptions(uri=str(input), storage_id='mcap'),
+        rosbag2_py.ConverterOptions(input_serialization_format='cdr', output_serialization_format='cdr'),
+    )
+
+    writer = rosbag2_py.SequentialWriter()
+    writer.open(
+        rosbag2_py.StorageOptions(uri=output, storage_id='mcap'),
+        rosbag2_py.ConverterOptions(input_serialization_format='cdr', output_serialization_format='cdr'),
+    )
+
+    # Create topics in writer, excluding filtered ones
+    for topic_metadata in reader.get_all_topics_and_types():
+        if topic_metadata.name not in topics_to_exclude:
+            writer.create_topic(topic_metadata)
+
+    # Copy messages, excluding filtered topics
+    while reader.has_next():
+        topic_name, data, timestamp = reader.read_next()
+        if topic_name not in topics_to_exclude:
+            writer.write(topic_name, data, timestamp)
